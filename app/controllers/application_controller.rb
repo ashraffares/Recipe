90ccfb95fc7a3e578be39d1ea0f8b29258ad5a0a
ashraffares <ashraffares090@gmail.com>
2021-11-20 13:58:24 +0200

    Add to sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery prepend: true
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :email, :password, :current_password)
+      end
 end
